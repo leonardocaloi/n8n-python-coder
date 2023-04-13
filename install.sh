747be89676580af39b129d98cd21bbca8f3c0a1b
Leonardo <santos.leonardocaloi@gmail.com>
2023-04-12 22:45:50 -0300

    fix abc user for coder

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 export $(grep -v '^#' .env | xargs)
 docker-compose down
 docker-compose up -d
-docker cp ./python/. code-server:"/home/${CODER_USER}/python_dir"
-docker cp ./init-coder.sh code-server:"/home/${CODER_USER}/python_dir/init-coder.sh"
-docker exec code-server /bin/bash -c "chmod +x /home/${CODER_USER}/python_dir/init-coder.sh && /home/${CODER_USER}/python_dir/init-coder.sh && rm /home/${CODER_USER}/python_dir/init-coder.sh"
+docker cp ./python/. code-server:"/home/${N8N_CODER_USER}/python_dir"
+docker cp ./init-coder.sh code-server:"/home/${N8N_CODER_USER}/python_dir/init-coder.sh"
+docker exec code-server /bin/bash -c "chmod +x /home/${N8N_CODER_USER}/python_dir/init-coder.sh && /home/${N8N_CODER_USER}/python_dir/init-coder.sh && rm /home/${N8N_CODER_USER}/python_dir/init-coder.sh"
